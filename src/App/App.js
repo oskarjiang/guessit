4158afee1f34f178d5aede29be4b3b5a2e51e6fe
unknown <oskarjiang94@gmail.com>
2019-04-23 20:03:27 +0200

    Added playlist chooser

--- src/App/App.js
+++ src/App/App.js
@@ -1,58 +1,84 @@
 import React, { Component } from 'react';
 import './App.css';
-import config from '../config.js';
 import Quiz from '../Quiz/Quiz';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
 const axios = require('axios')
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      tracks: undefined,
-      loginQueryParams: "https://accounts.spotify.com/authorize?client_id=e0597c6269bb4bcfbb0e811f3f6c937d&response_type=token&redirect_uri=http://localhost:3000"
+      loginQueryParams: "https://accounts.spotify.com/authorize?client_id=e0597c6269bb4bcfbb0e811f3f6c937d&response_type=token&redirect_uri=http://localhost:3000&scope=playlist-read-collaborative,playlist-read-private",
+      playListToUse: undefined,
+      userPlaylists: []
     };
+    this.setPlaylistToUse = this.setPlaylistToUse.bind(this);
   }
   componentDidMount() {
     const token = window.location.hash.substring(1).split('&')[0].split('=')[1];
-    if (token)
+    if (token){
       this.setState({
         requestHeaders: {
           "Accept": "application/json",
           "Content-Type": "application/json",
-          "Authorization": "Bearer "+token
+          "Authorization": "Bearer "+token,
         }
       });
+      this.setPlayLists(token)
+    }
   }
 
-  // Set state.tracks to top tracks
-  setTracksToUsersTop(){
+  // Sends a request for user's playlists, sets the state when finished
+  setPlayLists(token){    
     const request = axios.create({
-      baseURL: 'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10&offset=5',
-      headers: config.requestHeaders
+      baseURL: 'https://api.spotify.com/v1/me/playlists',
+      headers: {
+        "Authorization": "Bearer "+token,
+      }
     })
     request.get()
       .then((res) => {
-        let tracks = []
-        res.data.items.map(item => tracks.push(item.id))
-        this.setState({
-          tracks: tracks
-        })
+        this.setState({userPlaylists: res.data.items})
       })
       .catch((err) =>
-        console.error("Token is outdated"+err)
+         console.error("Error fetching playlists "+err)
       )
   }
+
+  // Sets the state with playlist chosen
+  setPlaylistToUse(e){
+    this.setState({playListToUse: e.target.id})
+  }
+
   render() {
-    if (undefined !== this.state.requestHeaders)
-      return (
+    if (undefined !== this.state.requestHeaders){
+      if (undefined !== this.state.userPlaylists)
+        if (undefined !== this.state.playListToUse)
+          // Render Quiz if playlist has been choosen
+          return (
+            <div className="App">
+                <Quiz
+                    playlistId={this.state.playListToUse}
+                    requestHeaders={this.state.requestHeaders}
+                />
+            </div>)
+        // Render playlist chooser if no playlist is chosen
+        return(
           <div className="App">
-              <Quiz
-                  playlistId='3Xt8b8Zs1fuZ0CkDsaPOdY'
-                  requestHeaders={this.state.requestHeaders}
-              />
+              {this.state.userPlaylists.map((playList, i) =>
+                <Row>
+                  <Button 
+                    id={playList.id}
+                    key={i}
+                    onClick={this.setPlaylistToUse}>
+                    {playList.name}
+                  </Button>
+                </Row>
+              )}
           </div>)
-      else
-          return(
-            <a href={this.state.loginQueryParams}>Login with Spotify</a>)
+    }
+    return(
+      <a href={this.state.loginQueryParams}>Login with Spotify</a>)
     ;
   }
 }
